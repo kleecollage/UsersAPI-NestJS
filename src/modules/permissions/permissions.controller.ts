1fbdd693e75df8e7e3b584d2e321e172c18b256b
Klee Collage <klee.collage@gmail.com>
2024-12-05 11:04:19 -0600

    feat(service, controller): Add POST endpoint for create permissions

--- src/modules/permissions/permissions.controller.ts
+++ src/modules/permissions/permissions.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PermissionDto } from 'src/modules/permissions/dto/permission-dto';
 import { PermissionsService } from 'src/modules/permissions/permissions.service';
 
-@Controller('permissions')
+@Controller('/api/v1/permissions')
 export class PermissionsController {
   constructor(private permissionsService: PermissionsService) {}
+
+  @Post()
+  createPermission(@Body() permission: PermissionDto) {
+    return this.permissionsService.createPermission(permission);
+  }
 }
--- src/modules/permissions/permissions.service.ts
+++ src/modules/permissions/permissions.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { PermissionDto } from 'src/modules/permissions/dto/permission-dto';
+import { Permission } from 'src/modules/permissions/schemas/permission.schema';
 
 @Injectable()
 export class PermissionsService {
-  constructor() {}
+  constructor(
+    @InjectModel(Permission.name) private permissionModel: Model<Permission>,
+  ) {}
+
+  async createPermission(permission: PermissionDto) {
+    const permissionExists = await this.permissionModel.findOne({
+      name: permission.name,
+    });
+
+    if (permissionExists) {
+      throw new ConflictException('Permission already exists');
+    }
+
+    const p = new this.permissionModel(permission);
+
+    return p.save();
+  }
 }
