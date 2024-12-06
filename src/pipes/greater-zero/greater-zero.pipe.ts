dc0c9872e78470f73f506de6e28da1abca779b98
Klee Collage <klee.collage@gmail.com>
2024-12-05 18:32:18 -0600

    feat(pipe): Add pipe greater-zero to transforms numbers or strings in positives only

---
+++ src/pipes/greater-zero/greater-zero.pipe.ts
@@ -0,0 +1,11 @@
+import { Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class GreaterZeroPipe implements PipeTransform {
+  transform(value: any) {
+    if (isNaN(+value) || +value <= 0) {
+      return 1;
+    }
+    return value;
+  }
+}
